87c45dd88e60e1634cc68aa3080488d3e5f3a940
Gitto K Tomy <gittokpr@gmail.com>
2022-03-14 09:15:44 +0530

    Split Screen Components Improvements.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import SplitScreen from "./SplitScreen";
 
-const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'green' }}>Left</h1>;
+const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
 }
 
-const RightHandComponent = () => {
-  return <h1 style={{ backgroundColor: 'red' }}>Right</h1>;
+const RightHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: 'red' }}>{name}</h1>;
 }
 
 function App() {
   return (
-    <SplitScreen left={LeftHandComponent} right={RightHandComponent} />
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name={"Sasi"} />
+      <RightHandComponent name={"3G"} />
+    </SplitScreen>
   );
 }
 
